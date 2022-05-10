452d7124f9018024cebdde1d3f334d71b068f377
TEMI <ahmet77meti@gmail.com>
2022-05-10 13:11:47 +0200

    asdasda

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'property',
+    'agent',
 ]
 
 MIDDLEWARE = [
@@ -126,7 +127,11 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+
+AUTH_USER_MODEL = 'agent.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
