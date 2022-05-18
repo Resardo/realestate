d39c2b8114cb3d791f31d21851a5da7eebb67929
RegiShtembari <99492951+RegiShtembari@users.noreply.github.com>
2022-05-18 14:14:02 +0200

    agjenti

--- agent/urls.py
+++ agent/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('/search/<slug:slug>/', views.agent_properties, name='agent_properties'),
-    path('/ourteam/', views.agent_all, name='agent_all'),
+    path('search/<slug:slug>/', views.agent_properties, name='agent_properties'),
+    path('ourteam/', views.agent_all, name='agent_all'),
 ]
