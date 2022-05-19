0fe3c9f99d94771f0c4306f4f882f051d9038b7b
temPirati <ahmet77meti@gmail.com>
2022-05-19 10:38:05 +0200

    pp

--- property/filters.py
+++ property/filters.py
@@ -1,12 +1,40 @@
+from argparse import Action
+from cProfile import label
 from dataclasses import fields
+from random import choices
+from xml.etree.ElementInclude import include
+from django import forms
 
 import django_filters
-from django_filters import DateFilter
 
-from .models import*
+
+from .models import *
+PROPERTY_TYPE = (
+        ("Apartament", "Apartament"),
+        ("Vila", "Vila"),
+        ("Store", "Dyqan"),
+        ("Land", "Truall"),
+        ("Garage", "Garazhd"),
+    )
+ACTION_OPTION = (
+        ("Sale", "Shitje"),
+        ("Rent", "Qera")
+    )
 
 class PropertyFilter(django_filters.FilterSet):
+    city = django_filters.ModelChoiceFilter(queryset=City.objects.all(), label="City")
+    district = django_filters.ModelChoiceFilter(queryset=District.objects.all(), label="District")
+    title = django_filters.CharFilter(lookup_expr='icontains', label="Title")
+    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt', label="Price min.")
+    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt', label="Price max.")
+    area__gt = django_filters.NumberFilter(field_name='area', lookup_expr='gt', label="Area min.")
+    area__lt = django_filters.NumberFilter(field_name='area', lookup_expr='lt', label="Area max.")
+    property_type = django_filters.ChoiceFilter(choices=PROPERTY_TYPE, label="Property Type")
+    activity = django_filters.ChoiceFilter(choices=ACTION_OPTION, label="Action")
+    
     class Meta:
         model = Property
-        fields = '__all__'
-        exclude = ['description','addres_line', 'area', 'views', 'documents', 'status', 'activity', 'updated_at','is_active','price' ]
+        fields = ['city', 'district','activity','property_type','title', 'price__gt', 'price__lt', 'area__gt', 'area__lt']
+        #label = ['City', 'District', 'Veprimi', 'Lloji prones', 'Titulli', 'Cmimi min.', 'Cmimi max.', 'Siperfaqja min.', 'Siperfaqja max.']
+        exclude = ['description','addres_line', 'views', 'documents', 'status', 'updated_at','is_active',]
+
